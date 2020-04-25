31de0da5e1ccbe1b51921d27655bb3cc087c1a07
Josh Young <j22cal@gmail.com>
2020-04-25 10:42:43 -0500

    Upgrade to rails version 5.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180610213056) do
+ActiveRecord::Schema.define(version: 2018_06_10_213056) do
 
   create_table "notes", force: :cascade do |t|
-    t.string   "subject"
-    t.string   "body"
+    t.string "subject"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
